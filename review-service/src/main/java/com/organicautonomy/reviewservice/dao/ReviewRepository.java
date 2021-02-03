4d10a1e10432777cebd38453743438e7ad43992c
brianthf <blora616@gmail.com>
2021-02-03 11:53:40 -0500

    start testing

---
+++ review-service/src/main/java/com/organicautonomy/reviewservice/dao/ReviewRepository.java
@@ -0,0 +1,16 @@
+package com.organicautonomy.reviewservice.dao;
+
+import com.organicautonomy.reviewservice.dto.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Repository
+public interface ReviewRepository extends JpaRepository<Review, Integer> {
+    List<Review> findReviewsByUserId(int userId);
+    @Query("SELECT * FROM review WHERE rating > ?1")
+    List<Review> findReviewsByRating(BigDecimal rating);
+}
---
+++ review-service/src/test/java/com/organicautonomy/reviewservice/dao/ReviewRepositoryTest.java
@@ -0,0 +1,21 @@
+package com.organicautonomy.reviewservice.dao;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReviewRepositoryTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void findReviewsByUserId() {
+    }
+
+    @Test
+    void findReviewsByRating() {
+    }
+}
