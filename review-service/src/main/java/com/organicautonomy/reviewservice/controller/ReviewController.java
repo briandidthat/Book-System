60908e1a11e9994ca7d84bbbe263d9ee949ce20e
brianthf <blora616@gmail.com>
2021-02-03 19:06:40 -0500

    controller testing --getAllReviews

--- review-service/src/main/java/com/organicautonomy/reviewservice/controller/ReviewController.java
+++ review-service/src/main/java/com/organicautonomy/reviewservice/controller/ReviewController.java
@@ -1,14 +1,26 @@
 package com.organicautonomy.reviewservice.controller;
 
 import com.organicautonomy.reviewservice.dao.ReviewRepository;
+import com.organicautonomy.reviewservice.dto.Review;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping("/reviews")
 public class ReviewController {
     @Autowired
     private ReviewRepository repository;
 
-
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<Review> getAllReviews() {
+        return repository.findAll();
+    }
 
 }
--- review-service/src/test/java/com/organicautonomy/reviewservice/controller/ReviewControllerTest.java
+++ review-service/src/test/java/com/organicautonomy/reviewservice/controller/ReviewControllerTest.java
@@ -1,20 +1,59 @@
 package com.organicautonomy.reviewservice.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.organicautonomy.reviewservice.dao.ReviewRepository;
 import com.organicautonomy.reviewservice.dto.Review;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(ReviewController.class)
 class ReviewControllerTest {
     private final Review TO_SAVE = new Review(1, 1, new BigDecimal("3.10"), "Ehh, not horrible.");
     private final Review REVIEW1 = new Review(1, 1, new BigDecimal("3.10"), "Ehh, not horrible.");
+    private final Review REVIEW2 = new Review(1, 2, new BigDecimal("4.80"), "Great book!");
+
+    @Autowired
+    private ObjectMapper mapper;
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private ReviewRepository repository;
 
     @BeforeEach
     void setUp() {
+    }
+
+    @Test
+    void testGetAllReviews() throws Exception {
+        List<Review> reviews = new ArrayList<>();
+        reviews.add(REVIEW1);
+        reviews.add(REVIEW2);
+
+        when(repository.findAll()).thenReturn(reviews);
+
+        String outputJson = mapper.writeValueAsString(reviews);
+
+        this.mockMvc.perform(get("/reviews"))
+                .andExpect(status().isOk())
+                .andExpect(content().json(outputJson))
+                .andDo(print());
     }
 }
