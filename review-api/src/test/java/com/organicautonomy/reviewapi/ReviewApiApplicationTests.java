0a099ff87649232e035613a9f4ef0ea6f557548f
brianthf <blora616@gmail.com>
2021-02-08 14:35:15 -0500

    initial review-api

---
+++ review-api/src/test/java/com/organicautonomy/reviewapi/ReviewApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.organicautonomy.reviewapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
