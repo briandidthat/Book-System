c58f16f1d5dc5349b79b4d9c4622584914e8ea61
brianthf <blora616@gmail.com>
2021-02-19 19:20:28 -0500

    rabbitmq

---
+++ review-api/src/main/java/com/organicautonomy/reviewapi/config/MessageConfig.java
@@ -0,0 +1,27 @@
+package com.organicautonomy.reviewapi.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MessageConfig {
+    public static final String EXCHANGE = "REVIEW_EXCHANGE";
+    public static final String ROUTING_KEY = "REVIEW_ROUTING_KEY";
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(jackson2JsonMessageConverter());
+        return rabbitTemplate;
+    }
+
+    @Bean
+    public Jackson2JsonMessageConverter jackson2JsonMessageConverter() {
+        ObjectMapper objectMapper = new ObjectMapper().findAndRegisterModules();
+        return new Jackson2JsonMessageConverter(objectMapper);
+    }
+}
